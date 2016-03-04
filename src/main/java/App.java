d849b58fa3e996bd7d20554a1045f495b7da1e24
Abigail Rolling <arolling@gmail.com>
2016-03-04 11:29:41 -0800

    Create and pass integration tests for adding bands and venues with display

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,9 +12,28 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      
+
+      model.put("allBands", Band.all());
+      model.put("allVenues", Venue.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/addBand", (request, response) -> {
+      String newBandName = request.queryParams("bandName");
+      Band newBand = new Band(newBandName);
+      newBand.save();
+      response.redirect("/");
+      return null;
+    });
+
+    post("/addVenue", (request, response) -> {
+      String newVenueName = request.queryParams("venueName");
+      int capacity = Integer.parseInt(request.queryParams("capacity"));
+      Venue newVenue = new Venue(newVenueName, capacity);
+      newVenue.save();
+      response.redirect("/");
+      return null;
+    });
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,15 +1,13 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
+import org.junit.*;
+import static org.junit.Assert.*;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
@@ -23,13 +21,27 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Rule
-  // public ClearRule clearRule = new ClearRule();
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
 
   //Integration testing
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Leap year detector");
-  // }
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Band Tracker");
+  }
+
+  @Test
+  public void addNewBandandVenue() {
+    goTo("http://localhost:4567/");
+    fill("#bandName").with("The Beatles");
+    submit("#addBand");
+    fill("#venueName").with("Shea Stadium");
+    fill("#capacity").with("80000");
+    submit("#addVenue");
+    assertThat(pageSource()).contains("The Beatles");
+    assertThat(pageSource()).contains("capacity 80000");
+  }
 }
